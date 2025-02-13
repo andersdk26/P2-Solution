7e726656d19bd73425d4ba0f0bfa4301744c6356
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2025-02-13 21:31:48 +0100

    Simple server script created

---
+++ back-end/server.js
@@ -0,0 +1,28 @@
+import { createServer } from 'node:http';
+import fs from 'node:fs';
+import url from 'node:url';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = createServer((req, res) => {
+    var query = url.parse(req.url, true);
+    if (query.pathname === "/" || query.pathname === "") {
+        query.pathname = '/index.html';
+    }
+    fs.readFile(`../test${query.pathname}`, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('404 Not Found');
+            console.log(err + '\nurl: ' + query.pathname);
+            return res.end();
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
