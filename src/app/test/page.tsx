deb3101740e37a3268b3c4f7236b48ddb54821b4
miagj02 <mjosef24@student.aau.dk>
2025-03-04 13:13:49 +0100

    Revert "Test/anders"

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,3 @@
-import Button from '@/components/Button';
 import Title from '@/components/Title';
 import { JSX } from 'react';
 
@@ -6,8 +5,6 @@ export default function test(): JSX.Element {
     return (
         <>
             <Title textColor="blue">Test</Title>
-            <Button className="m-8">Button</Button>
-            <Button className="m-8">Button</Button>
         </>
     );
 }
