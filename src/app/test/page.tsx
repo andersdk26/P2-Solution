dc31f8bbb7bcb037a55498c4f9376f1da9331620
JacobByrdal <GH12YF@student.aau.dk>
2025-03-04 11:52:49 +0100

    BigHARD button added

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Button';
 import Title from '@/components/Title';
 import { JSX } from 'react';
 
@@ -5,6 +6,8 @@ export default function test(): JSX.Element {
     return (
         <>
             <Title textColor="blue">Test</Title>
+            <Button className='m-8'>Click me HARD</Button>
+            <Button className='m-16'>Click me HARD</Button>
         </>
     );
 }
