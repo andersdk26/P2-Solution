a074ebbfe0ecdb16b37373b82a1f1689d462b708
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2025-03-10 09:55:31 +0100

    postsTable query added

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,57 +1,60 @@
 import { db } from 'db';
 import { postsTable, usersTable } from 'db/schema';
-import { JSX, useState } from 'react';
+import { JSX } from 'react';
 
 interface postsObjectProps {
     title: string;
     content: string;
 }
 
-export default function database(): JSX.Element {
-    const [lastPost, setLastPost] = useState<postsObjectProps | null>(null);
-
+export default async function database(): Promise<JSX.Element> {
+    const posts = await db.query.postsTable.findMany();
     return (
         <>
             <form
                 action={async () => {
                     'use server';
+                    const num = Math.ceil(Math.random() * 100000);
                     await db.insert(usersTable).values({
-                        id: 1,
+                        id: num,
                         age: 20,
-                        email: 'test@test.com',
-                        name: 'name',
+                        email: `test${num}@test.com`,
+                        name: `name${num}`,
                     });
                     await db.insert(postsTable).values({
                         title: 'Title',
-                        content: 'Content',
-                        userId: 1,
+                        content: num.toString(),
+                        userId: num,
                     });
                 }}
             >
                 <button type="submit">Submit</button>
             </form>
             <article>
-                <button
+                {/* <button
                     onClick={async () => {
                         const result = await db.query.postsTable.findFirst();
 
                         if (result) {
-                            setLastPost({
+                            lastPost = {
                                 title: result.title,
                                 content: result.content,
-                            });
+                            };
                         }
                     }}
                 >
                     GET
                 </button>
-                {lastPost ? (
+                {lastPost !== null && (
                     <p>
-                        Title: {lastPost.title}, content: {lastPost.content}
+                        Title: {lastPost!.title}, content: {lastPost!.content}
                     </p>
-                ) : (
-                    ''
-                )}
+                )} */}
+                {posts.map((post) => (
+                    <p key={post.content}>
+                        Title: {post!.title}, content: {post!.content}
+                    </p>
+                ))}
             </article>
         </>
     );
