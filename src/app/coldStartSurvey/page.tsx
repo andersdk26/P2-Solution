32ebcb7a3c708d513dc541aa2fc9f52bef1812b6
Svolle <64897616+TobiasSvoldgaard@users.noreply.github.com>
2025-03-13 16:51:06 +0100

    Movies are now selectable

--- src/app/coldStartSurvey/page.tsx
+++ src/app/coldStartSurvey/page.tsx
@@ -1,8 +1,27 @@
+'use client';
+
 import Image from 'next/image';
 import { JSX } from 'react';
+import { useState } from 'react';
 
 export default function MovieGrid(): JSX.Element {
+    // Create a state array for the movies displayed in the grid.
+    const [checkedMovies, setCheckedMovies] = useState<boolean[]>(
+        // Set all indicies to false.
+        Array(15).fill(false)
+    );
+
+    // Description of function.
+    const toggleCheck = (index: number) => {
+        setCheckedMovies((previousState) => {
+            const newCheckedMovies = [...previousState];
+            newCheckedMovies[index] = !newCheckedMovies[index];
+            return newCheckedMovies;
+        });
+    };
+
     return (
+        /// Create a div to center nested elements.
         <div className="flex items-center justify-center">
             <div className="grid grid-cols-5 grid-rows-3 gap-4 p-4">
                 {Array.from({ length: 15 }).map((_, index) => (
@@ -10,13 +29,14 @@ export default function MovieGrid(): JSX.Element {
                         <input
                             type="checkbox"
                             className="hidden"
-                            // className="appearance-none cursor-pointer border-2 border-white w-32 h-48 checked:bg-blue-500"
+                            onChange={() => toggleCheck(index)}
                         />
                         <Image
-                            width={128}
-                            height={192}
-                            src="/img/oppenheimer.png"
-                            alt="Oppenheimer movie poster"
+                            width={192}
+                            height={288}
+                            src={`/moviePosters/${index}.png`}
+                            alt="Movie poster."
+                            className={`border-4 ${checkedMovies[index] ? 'border-white' : 'border-transparent'} rounded-lg transition-all cursor-pointer`}
                         />
                     </label>
                 ))}
