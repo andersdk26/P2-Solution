206a07addca5d5e9ac35421ba5e4b4b319ccc663
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2025-03-14 00:19:26 +0100

    usersTable updated

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,28 +3,33 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const usersTable = sqliteTable('users', {
     id: integer('id').primaryKey(),
-    name: text('name').notNull(),
-    age: integer('age').notNull(),
+    username: text('username').notNull(),
     email: text('email').unique().notNull(),
-});
-
-export const postsTable = sqliteTable('posts', {
-    id: integer('id').primaryKey(),
-    title: text('title').notNull(),
-    content: text('content').notNull(),
-    userId: integer('user_id')
-        .notNull()
-        .references(() => usersTable.id, { onDelete: 'cascade' }),
+    password: integer('password').notNull(),
     createdAt: text('created_at')
         .default(sql`(CURRENT_TIMESTAMP)`)
         .notNull(),
-    updateAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(
-        () => new Date()
-    ),
+    lastLogin: text('last_login').default(sql`(CURRENT_TIMESTAMP)`),
+    settings: text('settings').default(sql`'{}'`), // JSON stringified object
 });
+
+// export const postsTable = sqliteTable('posts', {
+//     id: integer('id').primaryKey(),
+//     title: text('title').notNull(),
+//     content: text('content').notNull(),
+//     userId: integer('user_id')
+//         .notNull()
+//         .references(() => usersTable.id, { onDelete: 'cascade' }),
+//     createdAt: text('created_at')
+//         .default(sql`(CURRENT_TIMESTAMP)`)
+//         .notNull(),
+//     updateAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(
+//         () => new Date()
+//     ),
+// });
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+// export type InsertPost = typeof postsTable.$inferInsert;
+// export type SelectPost = typeof postsTable.$inferSelect;
