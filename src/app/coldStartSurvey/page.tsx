6d2559f0fc48a3702eee2c1091534795725bc772
Svolle <svolle777@icloud.com>
2025-03-18 17:29:53 +0100

    Selected movies now appear in separate section

--- src/app/coldStartSurvey/page.tsx
+++ src/app/coldStartSurvey/page.tsx
@@ -40,10 +40,11 @@ export default function MovieGrid(): JSX.Element {
                 Please select and rate at least five movies you&apos;ve watched
                 and enjoyed.
             </p>
-            <p className="text-center text-base pt-4">
+            <p className="text-center text-l pt-4">
                 You can always change your movie ratings under profile settings.
             </p>
 
+            {/* Search bar for finding movies */}
             <form className="max-w-1/3 mx-auto p-4">
                 <input
                     type="search"
@@ -53,16 +54,44 @@ export default function MovieGrid(): JSX.Element {
                 />
             </form>
 
-            {/* Pin selected movies to some sort of list. */}
-
-            {/* Create div for containing the grid. */}
+            {/* Create div for containing grids. */}
             <div className="flex-col items-center justify-center space-y-4">
-                <div className="w-1/2 mx-auto grid grid-cols-5 grid-rows-3 gap-4 p-8 bg-gray-100 rounded-3xl"></div>
-                {/* Define a 5x3 grid layout for the movies. */}
+                {/* Define a grid layout for selected movies. */}
+                <p className="text-center text-xl">Selected movies</p>
+                <div
+                    className={`w-1/2 mx-auto grid grid-cols-5 gap-4 p-8 bg-gray-100 rounded-3xl transition-all duration-300`}
+                    // Dynamically adjust height of selected movie box.
+                    style={{
+                        // Filter out false (unselected) movies, calculate number of rows (based on number of selected movies), multiply number of rows by movie poster height.
+                        minHeight: `${(Math.ceil(checkedMovies.filter((c) => c).length / 5) * 240, 240)}px`,
+                        maxHeight: '720px',
+                        overflowY: 'auto', // Show scrollbar when necessary when enough movies are shown.
+                    }}
+                >
+                    {/* Diplay movie posters of movies currently selected (based on their true/false state from the checkedMovies state array) */}
+                    {checkedMovies.map((isChecked, index) =>
+                        isChecked ? ( // If a movie is selected, display the movie poster.
+                            <Image
+                                key={index}
+                                src={`/moviePosters/${index}.png`}
+                                alt="Selected movie poster"
+                                width={160}
+                                height={240}
+                                className="rounded-2xl transition-all shadow-lg"
+                            />
+                        ) : // If a movie is not selected, do nothing.
+                        null
+                    )}
+                </div>
+                {/* Define a 5x3 grid layout for popular movies. */}
+                <p className="text-center text-xl">Popular movies</p>
                 <div className="w-1/2 mx-auto grid grid-cols-5 grid-rows-3 gap-4 p-8 bg-gray-100 rounded-3xl">
                     {/* Map an array to 15 checkboxes. */}
                     {Array.from({ length: 15 }).map((_, index) => (
-                        <label key={index} className="flex items-center gap-2">
+                        <label
+                            key={index}
+                            className="relative flex items-center gap-2"
+                        >
                             <input
                                 type="checkbox"
                                 // Hide the default checkbox.
@@ -73,12 +102,22 @@ export default function MovieGrid(): JSX.Element {
                             <Image
                                 // Display an image of the movie in place of the default checkbox.
                                 src={`/moviePosters/${index}.png`}
-                                alt="Movie poster."
+                                alt="Movie poster"
                                 width={160}
                                 height={240}
-                                // Toggle a blue border around the image when the movie is selected.
-                                className={`border-1 ${checkedMovies[index] ? 'border-blue-500 border-8' : 'border-transparent border-1'} rounded-2xl transition-all shadow-lg cursor-pointer`}
+                                // Toggle movie poster brightness.
+                                className={`border-none ${checkedMovies[index] ? 'brightness-25' : 'brightness-100'} rounded-2xl transition-all shadow-lg cursor-pointer`}
                             />
+                            {/* Show checkmark icon if movie is selected. */}
+                            {checkedMovies[index] && (
+                                <Image
+                                    src="/icons/checkmark-white.svg"
+                                    alt="Checkmark"
+                                    width={48}
+                                    height={48}
+                                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+                                />
+                            )}
                         </label>
                     ))}
                 </div>
