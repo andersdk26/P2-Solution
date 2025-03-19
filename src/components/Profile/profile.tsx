7fce3f1414276ced0a13a2f845ab3ed06be5f2cc
FrederikSkipper <RK23KN@student.aau.dk>
2025-03-19 15:31:01 +0100

    Redirrection function to the dropdown menu

--- src/components/Profile/profile.tsx
+++ src/components/Profile/profile.tsx
@@ -1,13 +1,22 @@
-// components/Profile/profileImg.tsx
+// /Users/frederikskipper-andersen/Documents/GitHub/P2-Solution/src/components/Profile/profile.tsx
 'use client';
 import React, { useState, JSX } from 'react'; // useState has isDropdown functions
 import ProfileImage from './profileImg';
+import { useRouter } from 'next/navigation'; // Import useRouter
 
 const Profile = (): JSX.Element => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const router = useRouter(); // Use the useRouter hook
 
     const toggleDropdown = (): void => {
         setIsDropdownOpen(!isDropdownOpen); // Toggle the dropdown on click
+    };
+
+    const redirrectProfile = (path: string): void => {
+        if (path) {
+            router.push(path);
+        }
+        setIsDropdownOpen(false);
     };
 
     return (
@@ -20,16 +29,28 @@ const Profile = (): JSX.Element => {
             </button>
             {isDropdownOpen && (
                 <div className="absolute top-16 right-0 bg-gray-900 text-gray-300 p-4 rounded-md w-48 my-3">
-                    <button className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1">
+                    <button
+                        onClick={() => redirrectProfile('/ProfileSettings')}
+                        className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1"
+                    >
                         Profile Settings
                     </button>
-                    <button className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1">
+                    <button
+                        onClick={() => redirrectProfile('/Watchlist')}
+                        className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1"
+                    >
                         Watchlist
                     </button>
-                    <button className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1">
+                    <button
+                        onClick={() => redirrectProfile('/Groups')}
+                        className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1"
+                    >
                         Groups
                     </button>
-                    <button className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1">
+                    <button
+                        onClick={() => redirrectProfile('/Logout')}
+                        className="flex items-center space-x-2 w-full p-2 hover:bg-gray-800 text-left my-1"
+                    >
                         Logout
                     </button>
                 </div>
