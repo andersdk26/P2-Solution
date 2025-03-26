0cb4149bd50d60b7d033cf76d983e6aeb4bb5ccb
Svolle <svolle777@icloud.com>
2025-03-26 14:51:07 +0100

    Updated rateMovies page skeleton

--- src/app/coldStartSurvey/rateMovies/page.tsx
+++ src/app/coldStartSurvey/rateMovies/page.tsx
@@ -15,9 +15,9 @@ export default function Home(): JSX.Element {
 
     // Dette array indeholder alle de film brugeren har valgt i første step af cold-start surveyen.
 
-    // Vis titel på film
-    // Vis filmplakat
-    // Popcorn rating knapper
-    // "Har ikke set" knap
-    // Finish/submit knap (skal tage en til vores main/homepage samt sende ratings til databasen)
+    // Vis titel på film samt hvilken film man er nået til, f.eks. Titanic (3/8).
+    // Vis filmplakat. Alternativt bare et placeholder billede, indtil vi kan hente filmplakater.
+    // Popcorn rating knapper til den viste film.
+    // "Har ikke set" knap.
+    // Finish/submit knap (skal tage en til vores main/homepage samt sende ratings til databasen).
 }
