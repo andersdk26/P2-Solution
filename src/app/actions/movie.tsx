cc6c8dd6cd907aea91879339d6b852e73a96657d
Svolle <svolle777@icloud.com>
2025-03-26 13:58:01 +0100

    Removed minimum length of search query to accommodate for short movie titles

--- src/app/actions/movie.tsx
+++ src/app/actions/movie.tsx
@@ -16,13 +16,8 @@ export async function searchForMovie(searchQuery: string): Promise<string[]> {
     // Sanitise search query.
     searchQuery = searchQuery.replace(/[^a-zA-Z0-9 ]/g, '');
 
-    // Only search if query is at least 3 letters long.
-    if (searchQuery.length < 3) {
-        return [];
-    }
-
     // Define sql query using Full-Text Search. Limited to 10 results.
-    const sql = `SELECT title FROM movies_fts WHERE title MATCH "${searchQuery}" LIMIT 10`;
+    const sql = `SELECT title FROM movies_fts WHERE title MATCH "${searchQuery}*" LIMIT 10`;
 
     // Fetch results.
     const result = await db.all<{ title: string }>(sql);
