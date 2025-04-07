d0de71b6737024eda5b6b333628c25501618b761
Svolle <svolle777@icloud.com>
2025-04-07 14:51:13 +0200

    Added user's username to page

---
+++ src/app/actions/logIn/username.ts
@@ -0,0 +1,15 @@
+'use server';
+
+import { db } from '@/db/index';
+import { usersTable } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+
+export default async function getUsername(
+    id: number | Promise<number>
+): Promise<string> {
+    const result = await db
+        .select({ username: usersTable.username })
+        .from(usersTable)
+        .where(eq(usersTable.id, await id));
+    return result[0].username;
+}
