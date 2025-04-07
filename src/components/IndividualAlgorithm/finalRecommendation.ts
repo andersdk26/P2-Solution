0d0cd8a2d4458e8059b0e9733edb78fd101992a3
FrederikSkipper <RK23KN@student.aau.dk>
2025-04-07 10:20:11 +0200

    should be done

--- src/components/IndividualAlgorithm/finalRecommendation.ts
+++ src/components/IndividualAlgorithm/finalRecommendation.ts
@@ -1,2 +1,31 @@
-/* Desciption */
+/* 
+Desciption: Need weights, need ERROR handling for fallback cases! Need to check SQL query for issues.
+ */
+import { eq, sql } from 'drizzle-orm';
+import scoreIndCollab from '@/components/IndividualAlgorithm/collaborative';
+import scoreIndContent from '@/components/IndividualAlgorithm/contentbased';
+import calculatePreferredGenres from '@/components/IndividualAlgorithm/preferredGenre';
+
+export async function finalRecommendation(
+    userId: string,
+    movieIds: number[]
+): Promise<{ movieId: number; score: number }[]> {
+    await calculatePreferredGenres(userId);
+
+    const moviesScores = [];
+    for (const movieId of movieIds) {
+        const scoreIndContentResult = await scoreIndContent(userId, movieId);
+        const scoreIndCollabResult = await scoreIndCollab(
+            movieId,
+            Number(userId)
+        );
+        const finalScore =
+            scoreIndContentResult.scoreIndContent + scoreIndCollabResult;
+        moviesScores.push({ movieId, finalScore });
+    }
+    moviesScores.sort((a, b) => b.finalScore - a.finalScore);
+    return [
+        { movieId: moviesScores[0].movieId, score: moviesScores[0].finalScore },
+    ]; // Return the first movie with the highest score
+}
 
