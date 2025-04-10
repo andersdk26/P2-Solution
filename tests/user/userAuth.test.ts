f42397561536932ae25bd943138fa221c2a7d8d3
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2025-04-10 09:54:51 +0200

    UserAuth tests added

--- tests/user/userAuth.test.ts
+++ tests/user/userAuth.test.ts
@@ -8,3 +8,21 @@ test('Loggin in for user "test"', async () => {
         object: { id: 5429199551 },
     });
 });
+
+test('Loggin in for user "test" with incorrect password', async () => {
+    expect(
+        await login_check({ username: 'test', password: 'Abcd1234' })
+    ).toStrictEqual({
+        status: 401,
+        message: 'Incorrect password',
+    });
+});
+
+test('Loggin in for unknown user', async () => {
+    expect(
+        await login_check({ username: 'qkblyadsf', password: 'abcd1234' })
+    ).toStrictEqual({
+        status: 404,
+        message: 'User not found',
+    });
+});
