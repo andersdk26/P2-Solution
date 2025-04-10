7fa5b2ed03320e6f0ca780f42642ced3814b1105
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2025-04-10 09:36:30 +0200

    Db and fetch lib added to jest

--- tests/user/userAuth.test.ts
+++ tests/user/userAuth.test.ts
@@ -1,7 +1,9 @@
 import { login_check } from '../../src/components/db/userAuth';
 
-test('Loggin in for user "test"', () => {
-    expect(login_check({ username: 'test', password: 'abcd1234' })).toBe({
+test('Loggin in for user "test"', async () => {
+    expect(
+        await login_check({ username: 'test', password: 'abcd1234' })
+    ).toStrictEqual({
         status: 200,
         object: { id: 5429199551 },
     });
