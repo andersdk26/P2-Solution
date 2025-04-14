0dfb2c3c47d7b569dabe91e1e1c623695a1f1410
miagj02 <mjosef24@student.aau.dk>
2025-04-14 13:21:29 +0200

    lidt blandet css

--- src/app/(with-navbar)/ProfileSettings/page.tsx
+++ src/app/(with-navbar)/ProfileSettings/page.tsx
@@ -96,194 +96,207 @@ export default function ProfileSettings() {
 
     return (
         <div className="p-8 text-black">
-            <h1 className="text-3xl font-bold mb-6">Profile Settings</h1>
+            <div className="ml-100 mr-100 pt-10 pb-10 rounded-sm bg-[#9fa3d1]">
+                <h1 className="mb-6 text-center">Profile Settings</h1>
 
-            <div className="flex flex-col items-center mb-8">
-                <Image
-                    src={selectedIcon}
-                    alt="Profile Icon"
-                    width={100}
-                    height={100}
-                    className="w-25 h-25 rounded-full border border-black object-cover select-none"
-                    draggable="false"
-                />
-                <p
-                    className="mt-2 text-blue-800 underline cursor-pointer"
-                    onClick={() => setIsPopupOpen(true)} // Open popup
-                >
-                    Change Profile Icon
-                </p>
-                <p className="text-xl mt-2 underline">
-                    {username || 'Loading...'}
-                </p>
-                <p className="text-sm">ID: #{id || 'Loading...'}</p>
-            </div>
+                <div className="flex flex-col items-center mb-8">
+                    <Image
+                        src={selectedIcon}
+                        alt="Profile Icon"
+                        width={100}
+                        height={100}
+                        className="w-25 h-25 rounded-full border border-black object-cover select-none"
+                        draggable="false"
+                    />
+                    <p
+                        className="mt-2 text-blue-800 underline cursor-pointer"
+                        onClick={() => setIsPopupOpen(true)} // Open popup
+                    >
+                        Change Profile Icon
+                    </p>
+                    <p className="text-xl mt-2 text-[#282f72] font-bold">
+                        {username || 'Loading...'}
+                    </p>
+                    <p className="text-sm text-[#282f72]">
+                        ID: #{id || 'Loading...'}
+                    </p>
+                </div>
 
-            {/* Popup til selecting profile icon */}
-            {isPopupOpen && (
-                <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50">
-                    <div className="bg-white p-6 rounded-lg shadow-lg">
-                        <h2 className="text-xl font-bold mb-4">
-                            Select Profile Icon
-                        </h2>
-                        <div className="grid grid-cols-3 gap-4">
-                            {icons.map((icon, index) => (
-                                <Image
-                                    key={index}
-                                    src={icon}
-                                    alt={`Icon ${index + 1}`}
-                                    width={64}
-                                    height={64}
-                                    className={`w-20 h-20 cursor-pointer rounded-full border select-none ${
-                                        selectedIcon === icon
-                                            ? 'border-blue-500'
-                                            : 'border-gray-300'
-                                    } hover:border-blue-500 object-cover`}
-                                    draggable="false"
-                                    onClick={() => setSelectedIcon(icon)}
-                                />
-                            ))}
-                        </div>
-                        <div className="flex justify-end mt-4 space-x-2">
-                            <button
-                                className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
-                                onClick={() => setIsPopupOpen(false)} // Close popup
-                            >
-                                Cancel
-                            </button>
-                            <button
-                                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-                                onClick={() => {
-                                    setIsPopupOpen(false);
-                                }}
-                            >
-                                Save
-                            </button>
+                {/* Popup til selecting profile icon */}
+                {isPopupOpen && (
+                    <div className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50">
+                        <div className="bg-white p-6 rounded-lg shadow-lg">
+                            <h2>Select Profile Icon</h2>
+                            <div className="grid grid-cols-3 gap-4">
+                                {icons.map((icon, index) => (
+                                    <Image
+                                        key={index}
+                                        src={icon}
+                                        alt={`Icon ${index + 1}`}
+                                        width={64}
+                                        height={64}
+                                        className={`w-20 h-20 cursor-pointer rounded-full border select-none ${
+                                            selectedIcon === icon
+                                                ? 'border-blue-500'
+                                                : 'border-gray-300'
+                                        } hover:border-blue-500 object-cover`}
+                                        draggable="false"
+                                        onClick={() => setSelectedIcon(icon)}
+                                    />
+                                ))}
+                            </div>
+                            <div className="flex justify-end mt-4 space-x-2">
+                                <button
+                                    className="bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400"
+                                    onClick={() => setIsPopupOpen(false)} // Close popup
+                                >
+                                    Cancel
+                                </button>
+                                <button
+                                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                                    onClick={() => {
+                                        setIsPopupOpen(false);
+                                    }}
+                                >
+                                    Save
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
+                )}
 
-            <p className="flex flex-col items-center font-bold mr-37 text-lg">
-                Username
-            </p>
-
-            <div className="flex flex-col items-center space-y-4 mb-10">
-                {/* Change Username Section */}
-                <p className="mb-0 flex flex-col items-center bg-[#282F72] text-[#dcdeef] font-bold px-6 py-2 rounded w-60">
-                    {username}
+                <p className="flex flex-col items-center font-bold mr-37 text-lg text-[#282f72]">
+                    Username
                 </p>
-                <div className="w-full flex flex-col items-center">
-                    <button
-                        className="mb-3 mr-26 underline text-blue-800 cursor-pointer select-none"
-                        onClick={() =>
-                            setIsEditing(
-                                isEditing === 'username' ? null : 'username'
-                            )
-                        }
-                    >
-                        {' '}
-                        Change Username
-                    </button>
-                    {isEditing === 'username' && (
-                        <div className="flex flex-col items-center mt-4">
-                            <input
-                                type="text"
-                                placeholder="Enter new username"
-                                value={newUsername}
-                                onChange={(e) => setNewUsername(e.target.value)}
-                                className="border p-2 rounded-md w-60 mb-2"
-                            />
 
-                            <button
-                                onClick={handleUsernameChange}
-                                className="bg-[#424ebd] text-white px-4 py-2 rounded-md hover:bg-pink-600 cursor-pointer"
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    )}
-                </div>
+                <div className="flex flex-col items-center space-y-4 mb-10">
+                    {/* Change Username Section */}
+                    <p className="mb-0 flex flex-col items-center bg-[#282F72] text-[#dcdeef] font-bold px-6 py-2 rounded w-60">
+                        {username}
+                    </p>
+                    <div className="w-full flex flex-col items-center">
+                        <button
+                            className="mb-3 mr-26 underline text-blue-800 cursor-pointer select-none"
+                            onClick={() =>
+                                setIsEditing(
+                                    isEditing === 'username' ? null : 'username'
+                                )
+                            }
+                        >
+                            {' '}
+                            Change Username
+                        </button>
+                        {isEditing === 'username' && (
+                            <div className="flex flex-col items-center mt-4">
+                                <input
+                                    type="text"
+                                    placeholder="Enter new username"
+                                    value={newUsername}
+                                    onChange={(e) =>
+                                        setNewUsername(e.target.value)
+                                    }
+                                    className="border p-2 rounded-md w-60 mb-2"
+                                />
 
-                {/* Password Section */}
-                <p className="font-bold mb-0 mr-38 text-lg">Password</p>
-                <p className="mb-0 flex flex-col items-center bg-[#282F72] text-[#dcdeef] font-bold px-6 py-2 rounded w-60">
-                    Password
-                </p>
-                <div className="w-full flex flex-col items-center">
-                    <button
-                        className="mb-3 mr-26 underline text-blue-800 cursor-pointer select-none"
-                        onClick={() =>
-                            setIsEditing(
-                                isEditing === 'password' ? null : 'password'
-                            )
-                        }
-                    >
-                        Change Password
-                    </button>
-                    {isEditing === 'password' && (
-                        <div className="flex flex-col items-center mt-4">
-                            <input
-                                type="password"
-                                placeholder="Enter current password"
-                                value={currentPassword}
-                                onChange={(e) =>
-                                    setCurrentPassword(e.target.value)
-                                }
-                                className="border p-2 rounded-md w-60 mb-2"
-                            />
-                            <input
-                                type="password"
-                                placeholder="Enter new password"
-                                value={newPassword}
-                                onChange={(e) => setNewPassword(e.target.value)}
-                                className="border p-2 rounded-md w-60 mb-2"
-                            />
-                            <button
-                                onClick={handlePasswordChange}
-                                className="bg-[#424ebd] text-white px-4 py-2 rounded-md hover:bg-pink-600 cursor-pointer"
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    )}
-                </div>
+                                <button
+                                    onClick={handleUsernameChange}
+                                    className="basicBtn"
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        )}
+                    </div>
 
-                <p className="font-bold mb-0 mr-47 text-lg">Email</p>
+                    {/* Password Section */}
+                    <p className="font-bold mb-0 mr-38 text-lg text-[#282f72]">
+                        Password
+                    </p>
+                    <p className="mb-0 flex flex-col items-center bg-[#282F72] text-[#dcdeef] font-bold px-6 py-2 rounded w-60">
+                        Password
+                    </p>
+                    <div className="w-full flex flex-col items-center">
+                        <button
+                            className="mb-3 mr-26 underline text-blue-800 cursor-pointer select-none"
+                            onClick={() =>
+                                setIsEditing(
+                                    isEditing === 'password' ? null : 'password'
+                                )
+                            }
+                        >
+                            Change Password
+                        </button>
+                        {isEditing === 'password' && (
+                            <div className="flex flex-col items-center mt-4">
+                                <input
+                                    type="password"
+                                    placeholder="Enter current password"
+                                    value={currentPassword}
+                                    onChange={(e) =>
+                                        setCurrentPassword(e.target.value)
+                                    }
+                                    className="border p-2 rounded-md w-60 mb-2"
+                                />
+                                <input
+                                    type="password"
+                                    placeholder="Enter new password"
+                                    value={newPassword}
+                                    onChange={(e) =>
+                                        setNewPassword(e.target.value)
+                                    }
+                                    className="border p-2 rounded-md w-60 mb-2"
+                                />
+                                <button
+                                    onClick={handlePasswordChange}
+                                    className="basicBtn"
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        )}
+                    </div>
 
-                {/* Change Email Section */}
-                <p className="mb-0 flex flex-col items-center bg-[#282F72] text-[#dcdeef] font-bold px-6 py-2 rounded w-60">
-                    {email}
-                </p>
-                <div className="w-full flex flex-col items-center">
-                    <button
-                        className="mb-0 mr-33 underline text-blue-800 cursor-pointer select-none"
-                        onClick={() =>
-                            setIsEditing(isEditing === 'email' ? null : 'email')
-                        }
-                    >
-                        Change Email
-                    </button>
-                    {isEditing === 'email' && (
-                        <div className="flex flex-col items-center mt-4">
-                            <input
-                                type="email"
-                                placeholder="Enter new email"
-                                value={newEmail}
-                                onChange={(e) => setNewEmail(e.target.value)}
-                                className="border p-2 rounded-md w-60 mb-2"
-                            />
-                            <button
-                                onClick={handleEmailChange}
-                                className="bg-[#424ebd] text-white px-4 py-2 rounded-md hover:bg-pink-600 cursor-pointer"
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    )}
+                    <p className="font-bold mb-0 mr-47 text-lg text-[#282f72]">
+                        Email
+                    </p>
+
+                    {/* Change Email Section */}
+                    <p className="mb-0 flex flex-col items-center bg-[#282F72] text-[#dcdeef] font-bold px-6 py-2 rounded w-60">
+                        {email}
+                    </p>
+                    <div className="w-full flex flex-col items-center">
+                        <button
+                            className="mb-0 mr-33 underline text-blue-800 cursor-pointer select-none"
+                            onClick={() =>
+                                setIsEditing(
+                                    isEditing === 'email' ? null : 'email'
+                                )
+                            }
+                        >
+                            Change Email
+                        </button>
+                        {isEditing === 'email' && (
+                            <div className="flex flex-col items-center mt-4">
+                                <input
+                                    type="email"
+                                    placeholder="Enter new email"
+                                    value={newEmail}
+                                    onChange={(e) =>
+                                        setNewEmail(e.target.value)
+                                    }
+                                    className="border p-2 rounded-md w-60 mb-2"
+                                />
+                                <button
+                                    onClick={handleEmailChange}
+                                    className="basicBtn"
+                                >
+                                    Submit
+                                </button>
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
-
             <div className="ml-12">
                 <p className="text-lg font-semibold">User statsss</p>
                 <ul className="list-disc list-inside ml-4">
