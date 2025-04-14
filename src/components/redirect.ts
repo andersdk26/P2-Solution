414772df30dfb49c679753f564818c00ed62dba6
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2025-04-14 23:51:05 +0200

    Redirect client side updated

--- src/components/redirect.ts
+++ src/components/redirect.ts
@@ -1,29 +1,19 @@
 'use client';
 
-import { config } from 'dotenv';
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
-config({ path: '.env' }); // or .env.local
+export default function useRedirect() {
+    const router = useRouter();
 
-export default function redirectClient(
-    path: string,
-    addToHistory: boolean = true
-): void {
-    const rootPath = process.env.URL_PATH || '/';
+    return (path: string, addToHistory: boolean = true) => {
+        const rootPath = process.env.URL_PATH || '/';
+        const cleanPath = path.startsWith('/') ? path.slice(1) : path;
+        const finalPath = rootPath + cleanPath;
 
-    // Remove leading slash
-    if (path && path[0] === '/') {
-        path = path.substring(1);
-    } else if (!path) {
-        path = '';
-    }
-
-    // Push last page to browser window history
-    if (addToHistory) {
-        // TODO something
-        // window.history.pushState({ pathname: rootPath }, '');
-    }
-
-    // Redirect including PATH variable
-    redirect(rootPath + path);
+        if (addToHistory) {
+            router.push(finalPath); // Tilføjer til historik
+        } else {
+            router.replace(finalPath); // Overskriver historik
+        }
+    };
 }
