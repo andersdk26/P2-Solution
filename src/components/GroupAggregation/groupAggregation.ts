66d3c357d1789b09c8c773235f5bec2f55b67134
Svolle <svolle777@icloud.com>
2025-04-23 12:53:12 +0200

    Small changes

--- src/components/GroupAggregation/groupAggregation.ts
+++ src/components/GroupAggregation/groupAggregation.ts
@@ -2,7 +2,7 @@
 
 import { moviesTable, groupsTable, testRatings } from '@/db/schema';
 import { db } from 'db';
-import { eq, ne } from 'drizzle-orm';
+import { eq } from 'drizzle-orm';
 
 export default async function groupAggregation(groupId: number) {
     // Get group members' ID.
@@ -17,9 +17,23 @@ export default async function groupAggregation(groupId: number) {
     const individualIds = memberIds[0].members.split('|');
 
     // Map of group members average ratings.
+    const averageGroupRatings = new Map<number, number>();
 
     // Iterate through each group member's ID.
     for (const id of individualIds) {
+        // Get their respective ratings.
+        const memberRatings = await db
+            .select({
+                userId: testRatings.userId,
+                movieId: testRatings.movieId,
+                movieRating: testRatings.rating,
+            })
+            .from(testRatings)
+            .where(eq(testRatings.userId, parseInt(id)));
+
+        // Add each rating to 'averageGroupRatings'.
+        for (const rating of memberRatings) {
+        }
     }
 
     return 0;
