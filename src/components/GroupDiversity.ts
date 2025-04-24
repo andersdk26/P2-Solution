19a187a439f0bc5fb808fe388c180be02aa81814
FrederikSkipper <RK23KN@student.aau.dk>
2025-04-24 09:35:02 +0200

    Draft

---
+++ src/components/GroupDiversity.ts
@@ -0,0 +1,21 @@
+/* Should return a similarityscore for a group
+used for weights contentbased % + collaborative % */
+
+
+CalculateGroupDiversity(contentBasedFiltering) {
+    const DiversityMovies: movie[] = [];
+   for (UserID in Group) {
+       contentBasedFiltering(UserID);
+           DiversityMovies.push(m);
+       }
+       for (movieID in DiversityMovies ) {
+           SELECT genre FROM tabel;
+               DiversityMovies.push(m);
+       }
+       for (genre in DiversityMovies) {
+           DiversityMovies.count();
+           DiversityMovies.Sort();
+       }
+       Similarity() => {}
+   }
+   
