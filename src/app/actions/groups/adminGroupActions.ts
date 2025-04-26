1b15ecbe2f33913ae1d4d9cd240e914233a7631c
123itismesara <sfalch24@student.aau.dk>
2025-04-26 20:06:51 +0200

    add group members

---
+++ src/app/actions/groups/adminGroupActions.ts
@@ -0,0 +1,26 @@
+'use server';
+
+import { db } from '@/db/index';
+import { groupsTable } from '@/db/schema';
+import { and, eq, like, not } from 'drizzle-orm';
+
+// Add user to group
+export async function AddUserToGroup(
+    groupId: number,
+    groupMembers: string,
+    addedUserId: number
+): Promise<void> {
+    await db
+        .update(groupsTable)
+        .set({ members: `${groupMembers}|${addedUserId}` })
+        .where(
+            // the group id and that the user id does not already exist in group members
+            and(
+                eq(groupsTable.groupId, groupId),
+                not(like(groupsTable.groupName, `${addedUserId}`))
+            )
+        );
+
+    console.log(addedUserId);
+    console.log('User added to group');
+}
--- src/components/groupPage/adminGroupIcon.tsx
+++ src/components/groupPage/adminGroupIcon.tsx
@@ -3,6 +3,8 @@ import { JSX, useEffect } from 'react';
 import { useState } from 'react';
 import { group } from './group';
 import getUserById from '@/actions/friends/getUserById';
+import { searchUserById, user } from '../Profile/Friends/friends';
+import { AddUserToGroup } from '@/actions/groups/adminGroupActions';
 
 export default function AdminGroupIcon({
     groupId,
@@ -19,6 +21,8 @@ export default function AdminGroupIcon({
     const [MemberUsernames, setMemberUsernames] = useState<string[]>([]);
     //the username of the admin
     const [AdminUsername, setAdminUsername] = useState('');
+    // search result
+    const [searchResult, setSearchResult] = useState<user[]>([]);
 
     // make array with the settings
     const settingsList = settings.split('|');
@@ -47,6 +51,14 @@ export default function AdminGroupIcon({
         };
         getAdminName();
     });
+
+    const handleAddUserToGroup = (addedUser: user) => {
+        AddUserToGroup(groupId, groupMembers, addedUser.userId);
+        // notify that there has been a success
+        alert(`${addedUser} has been added to your group!`);
+        // close the pop up for the add friend
+        setAddFriendsOpen(false);
+    };
 
     return (
         <>
@@ -132,7 +144,12 @@ export default function AdminGroupIcon({
 
                             <br />
                             {/* Invite group members */}
-                            <button className="bg-green-500 text-black m-4 ml-0 p-2 rounded-sm bottom-4 cursor-pointer hover:brightness-80">
+                            <button
+                                className="bg-green-500 text-black m-4 ml-0 p-2 rounded-sm bottom-4 cursor-pointer hover:brightness-80"
+                                onClick={() => {
+                                    setAddFriendsOpen(true);
+                                }}
+                            >
                                 Add members
                             </button>
 
@@ -181,6 +198,70 @@ export default function AdminGroupIcon({
                         </div>
                     </div>
                 </section>
+            )}
+            {/* the information about groups in the box */}
+            {isAddFriendsOpen && (
+                <aside
+                    className={`fixed top-4 left-0 z-40 w-screen h-screen flex items-center justify-center`}
+                >
+                    {/* the box container */}
+                    <div
+                        className={`z-50 w-2/3 h-1/2 border-2 border-solid border-[#282F72] bg-[#9fa3d1] rounded-3xl m-4 align-center items-center overflow-scroll`}
+                    >
+                        {/* the close button */}
+                        <button
+                            className="cursor-pointer float-right right-4 top-3 mr-4 mt-2 mb-0 z-50 cursor-pointer text-2xl hover:opacity-85 "
+                            onClick={() => {
+                                setAddFriendsOpen(false);
+                            }}
+                        >
+                            <u>Close</u>
+                        </button>
+
+                        <div className="fixed mt-10 left-2/5 content-center align-center items-center">
+                            {/* the search form */}
+                            <form
+                                className="w-80 justify-stretch py-2 text-black "
+                                onSubmit={(e) => e.preventDefault()}
+                            >
+                                <input
+                                    type="search"
+                                    id="UserSearch"
+                                    className="block w-full p-2 px-4 rounded-full bg-gray-100"
+                                    placeholder="Search for users..."
+                                    // When the user types something, call function to fetch movies with matching search query.
+                                    onChange={async (e) => {
+                                        setSearchResult(
+                                            await searchUserById(e.target.value)
+                                        );
+                                    }}
+                                />
+                            </form>
+                            {/* section for the search results */}
+                            <aside
+                                id="searchResults"
+                                className="top-87 w-80 bg-gray-100 rounded-3xl max-h-50 overflow-scroll"
+                            >
+                                {searchResult.map((user) => (
+                                    // movieId is used as identifier as it ensures that each item has a unique key.
+                                    <div key={user.userId}>
+                                        <p
+                                            onClick={() => {
+                                                // set the current selected user to the user that is clicked
+                                                handleAddUserToGroup(user);
+                                            }}
+                                            className={`py-2 px-4 justify-between hover:bg-blue-500 hover:text-white rounded-3xl cursor-pointer`}
+                                        >
+                                            <span className="text-left text-black prevent-select">
+                                                {user.userName}
+                                            </span>
+                                        </p>
+                                    </div>
+                                ))}
+                            </aside>
+                        </div>
+                    </div>
+                </aside>
             )}
         </>
     );
