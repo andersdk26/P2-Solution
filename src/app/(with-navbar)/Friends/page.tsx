e1d3e496a36bcd003562c1acf0df1ca3eea90f31
miagj02 <mjosef24@student.aau.dk>
2025-04-30 14:58:06 +0200

    remove friend confirm pop-up

--- src/app/(with-navbar)/Friends/page.tsx
+++ src/app/(with-navbar)/Friends/page.tsx
@@ -13,6 +13,11 @@ import getUserID from '@/actions/logIn/userID';
 import { GetFriends } from '@/actions/friends/friendsList';
 
 export default function Friends(): JSX.Element {
+    // Pop up for unfriending someone
+    const [unfriendOpen, setUnfriendOpen] = useState(0);
+
+    const [userId, setUserId] = useState('User ID#');
+
     const [FriendRequests, setFriendRequests] = useState<{ from: number }[]>(
         []
     );
@@ -27,6 +32,27 @@ export default function Friends(): JSX.Element {
             No friends to show.
         </p>,
     ]);
+    const [unfriendName, setUnfriendName] = useState('');
+
+    // fetching of your own userId to show the user
+    useEffect(() => {
+        const fetchUserId = async (): Promise<void> => {
+            setUserId(String(await getUserID(verifyUser()))); // setUserId recieves the functions of getUserID(verifyuser)
+        };
+        fetchUserId();
+    }, []);
+
+    useEffect(() => {
+        const getName = async () => {
+            if (unfriendOpen === 0) {
+                setUnfriendName('');
+                return;
+            }
+
+            setUnfriendName(await getUserById(unfriendOpen));
+        };
+        getName();
+    }, [unfriendOpen]);
 
     const getFriendRequests = async (): Promise<void> => {
         setFriendRequests(await GetFriendRequest(await verifyUser()));
@@ -97,7 +123,7 @@ export default function Friends(): JSX.Element {
             const resolvedFriends = await Promise.all(
                 FriendsList.map(async (id) => (
                     <div
-                        className="flex items-center space-x-2 bg-[#9fa3d1] mr-25 mb-4 p-4 rounded-sm" //HERRRRRRR
+                        className="flex items-center space-x-2 bg-[#9fa3d1] mr-25 mb-4 p-4 rounded-sm"
                         key={id}
                     >
                         <p className="ml-4 my-auto w-64 py-4">
@@ -105,12 +131,8 @@ export default function Friends(): JSX.Element {
                         </p>
                         <button
                             className="bg-[#db0000] hover:bg-[#b00000] text-[#ffffff] font-bold my-auto py-2 px-4 mr-2 ml-auto relative rounded-sm cursor-pointer"
-                            onClick={async () => {
-                                RemoveFriend(await verifyUser(), id);
-                                alert(
-                                    `${await getUserById(id)} has been removed from your friends!`
-                                );
-                                getFriendsList();
+                            onClick={() => {
+                                setUnfriendOpen(id);
                             }}
                         >
                             Remove friend
@@ -132,7 +154,7 @@ export default function Friends(): JSX.Element {
                     <section className="h-auto m-4 p-4 bg-[#babdde] rounded-sm">
                         <h2>Your friends</h2>
                         <p className="ml-4 text-[#282f72]">
-                            Here you can view and manage your friends.
+                            Here, you can view and manage your friends.
                         </p>
                         <div className="pl-4 pt-4 text-[#282f72]">
                             {friendsListObject}
@@ -145,6 +167,9 @@ export default function Friends(): JSX.Element {
                         <p className="ml-4 text-[#282f72]">
                             Search for users by their user ID or their username
                             to send them a friend request.
+                        </p>
+                        <p className="ml-4 text-[#282f72]">
+                            Your user ID: #{userId}
                         </p>
                         <aside className="align-left content-left justify-left text-left table ml-4 rounded-sm">
                             <SearchFriends />
@@ -164,6 +189,54 @@ export default function Friends(): JSX.Element {
                     </section>
                 </section>
             </section>
+            {/* pop-up to confirm to unfriend*/}
+            {unfriendOpen && (
+                <aside
+                    className={`fixed top-4 left-0 z-40 w-2/3 h-2/3 flex items-center justify-center`}
+                    onLoad={() => alert(unfriendOpen)}
+                >
+                    {/* the box container */}
+                    <div
+                        className={`z-50 w-2/3 h-1/2 border-2 border-solid border-[#282F72] bg-[#9fa3d1] rounded-3xl m-4`}
+                    >
+                        {/*the close button*/}
+                        <button
+                            className="cursor-pointer float-right right-4 top-3 mr-4 mt-2 mb-0 z-50 cursor-pointer text-2xl hover:opacity-85 "
+                            onClick={() => {
+                                setUnfriendOpen(0);
+                            }}
+                        >
+                            <u>Close</u>
+                        </button>
+                        {/* Affirmation of unfriending-part */}
+                        <div className="align-center items-center content-center text-center mt-4">
+                            <p className="text-3xl text-center">
+                                Are you sure you want to unfriend {unfriendName}
+                                ?
+                            </p>
+                            <button
+                                className="bg-[#db0000] hover:bg-[#b00000] text-[#ffffff] font-bold my-auto py-2 px-4 p-5 mt-20 rounded-sm cursor-pointer"
+                                onClick={async () => {
+                                    // removes the specific friend, when clicked
+                                    RemoveFriend(
+                                        await verifyUser(),
+                                        unfriendOpen
+                                    );
+                                    alert(
+                                        `${unfriendName} has been removed from your friends!`
+                                    );
+
+                                    getFriendsList(); // updates friendlist
+                                    setUnfriendOpen(0); // closes pop-up
+                                    location.reload(); // reloads page
+                                }}
+                            >
+                                Remove {unfriendName}
+                            </button>
+                        </div>
+                    </div>
+                </aside>
+            )}
         </>
     );
 }
