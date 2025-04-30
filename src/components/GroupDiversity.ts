07cbdc1403831884e0793a476fc0b03216b8755d
FrederikSkipper <RK23KN@student.aau.dk>
2025-04-30 14:11:35 +0200

    fetched latest genreScoreMap  with an average score

--- src/components/GroupDiversity.ts
+++ src/components/GroupDiversity.ts
@@ -7,12 +7,14 @@ import { db } from 'db';
 import { eq, ne } from 'drizzle-orm';
 import { getNamedRouteRegex } from 'next/dist/shared/lib/router/utils/route-regex';
 
+let targetUserRatings;
 export default async function similarityscore(
     genreScoreMap: Map<string, number>,
     group: number
 ): Promise<number> {
     // fetch groupId and users from group table. (MISSING)
     let similarityScore: number = 0;
+    let userId: number = targetUserRatings;
     // new map with userId as key and genre score map as value
     const groupGenreScoreMap = new Map<number, Map<string, number>>();
     for (const userId of group) {
@@ -21,64 +23,92 @@ export default async function similarityscore(
     }
     // aggregate genre difference for each genre (MISSING)
     for (const [userId, genreScoreMap] of groupGenreScoreMap) {
-    }
-
-    /*  maybe sort the map first so we have the highest genre score first in the genreScoreMap. 
+        for (const genre of genreScoreMap) {
+        }}
+        /*  maybe sort the map first so we have the highest genre score first in the genreScoreMap. 
     (we cant only compare genrescore: since we aggregate the scores which means someones favorite genre can be the same as someone else, but if one has rated many more movies
     then it would indicate a low similarity score, which is not what we want)
     */
-    return similarityScore;
-}
-
-async function genreData(userId: number): Promise<Map<string, number>> {
-    // Initialise genre score map.
-    const genreScoreMap = new Map<string, number>();
-
-    // Fetch the user's ratings.
-    const userRatings = await db
-        .select({
-            userId: testRatings.userId,
-            movieId: testRatings.movieId,
-            movieRating: testRatings.rating,
-        })
-        .from(testRatings)
-        .where(eq(testRatings.userId, userId));
-
-    // Iterate through each rating made by the user.
-    for (const rating of userRatings) {
-        // Fetch the movie associated with the rating.
-        const movie = await getMovieById(rating.movieId);
-
-        // Check that the movie exists.
-        if (movie !== null) {
-            // Get genres of the movie.
-            const genres = movie.movieGenres;
-
-            // If movie has no genres, continue to the next rating/movie.
-            if (genres === '(no genres listed)') {
-                continue;
-            }
-
-            // Split string into individual genres.
-            const genreArray = genres.split('|');
-
-            // Iterate through each genre.
-            for (const genre of genreArray) {
-                // If the genre score map does not contain an entry for the specified genre, initialise it.
-                if (!genreScoreMap.has(genre)) {
-                    genreScoreMap.set(genre, 0);
-                }
-
-                // Update the score for the current genre.
-                genreScoreMap.set(
-                    genre,
-                    genreScoreMap.get(genre)! + rating.movieRating
-                );
-            }
-        }
+        return similarityScore;
     }
 
-    // Return the genre score map.
-    return genreScoreMap;
+    type averageRating = {
+        runningTotal: number;
+        timesRated: number;
+    };
+
+    async function genreData(targetId: number): Promise<Map<string, number>> {
+        // Declare variable used for storing movie ratings.
+        let targetUserRatings;
+        targetUserRatings = await db
+            .select({
+                userId: testRatings.userId,
+                movieId: testRatings.movieId,
+                movieRating: testRatings.rating,
+            })
+            .from(testRatings)
+            .where(eq(testRatings.userId, targetId));
+
+        console.log('Got target user ratings.');
+        console.log(targetUserRatings);
+
+        // Find the total number of movies rated by the target user.
+        const totalMoviesRated = targetUserRatings!.length;
+
+        // Initialise genre score map.
+        const averageGenreRating = new Map<string, averageRating>();
+
+        // Iterate through each rating made by the target user.
+        for (const rating of targetUserRatings!) {
+            // Fetch the movie in question.
+            const movie = await getMovieById(rating.movieId);
+
+            // Check that the movie exists.
+            if (movie !== null) {
+                // Get genres of the movie.
+                const genres = movie.movieGenres;
+
+                // If movie has no genres, continue to the next rating/movie.
+                if (genres === '(no genres listed)') {
+                    continue;
+                }
+
+                // Split string into single genres.
+                const genreArray = genres.split('|');
+
+                // Iterate through each genre.
+                for (const genre of genreArray) {
+                    // If the genre score map does not contain an entry for the specified genre, initialise it.
+                    if (!averageGenreRating.has(genre)) {
+                        averageGenreRating.set(genre, {
+                            runningTotal: 0,
+                            timesRated: 0,
+                        });
+                    }
+
+                    // Update the score for the current genre.
+                    averageGenreRating.set(genre, {
+                        runningTotal:
+                            averageGenreRating.get(genre)!.runningTotal +
+                            rating.movieRating,
+                        timesRated:
+                            averageGenreRating.get(genre)!.timesRated + 1,
+                    });
+                }
+            }
+        }
+
+        // Convert running total and times rated to a score for each genre.
+        const genreScoreMap = new Map<string, number>();
+
+        for (const genre of averageGenreRating) {
+            genreScoreMap.set(
+                genre[0],
+                (genre[1].runningTotal / genre[1].timesRated) *
+                    (genre[1].timesRated / totalMoviesRated)
+            );
+        }
+        return genreScoreMap;
+    }
 }
 
