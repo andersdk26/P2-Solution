27a119e987f89150db9d136660f2af66eb0ccc8c
andersdk26 <98921582+andersdk26@users.noreply.github.com>
2025-05-05 10:08:05 +0200

    MySQL tables addded

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,121 +1,49 @@
-// import { sql } from 'drizzle-orm';
-// import { bigint } from 'drizzle-orm/mysql-core';
-// import {
-//     int,
-//     mysqlTable,
-//     serial,
-//     timestamp,
-//     varchar,
-// } from 'drizzle-orm/mysql-core';
-
-// // *** Movie ***
-// export const moviesTable = mysqlTable('movies', {
-//     id: int('id').primaryKey(),
-//     title: varchar('title', { length: 255 }).notNull(),
-//     genres: varchar('genres', { length: 255 }).notNull(),
-// });
-
-// export type InsertMovie = typeof moviesTable.$inferInsert;
-// export type SelectMovie = typeof moviesTable.$inferSelect;
-
-// export const movieLinkIdTable = mysqlTable('movie_link_id', {
-//     id: int('id').primaryKey(),
-//     imdbId: int('imdbId').notNull(),
-//     tmdbId: int('tmdbId').notNull(),
-// });
-
-// export type InsertMovieLinkId = typeof movieLinkIdTable.$inferInsert;
-// export type SelectMovieLinkId = typeof movieLinkIdTable.$inferSelect;
-
-// export const IMDBImageIdTable = mysqlTable('imdb_image_id', {
-//     id: int('id').primaryKey(),
-//     imageId: int('imageId', { unsigned: true }).notNull(),
-// });
-
-// export type InsertIMDBImageId = typeof IMDBImageIdTable.$inferInsert;
-// export type SelectIMDBImageId = typeof IMDBImageIdTable.$inferSelect;
-
-// export const movieImageCacheTable = mysqlTable('movie_image_cache', {
-//     id: serial('id').primaryKey(), // auto increment
-//     movieId: int('movieId')
-//         .notNull()
-//         .references(() => moviesTable.id),
-//     url: varchar('url', { length: 255 }).notNull(),
-//     blurHash: varchar('blurHash', { length: 255 }),
-//     timestamp: timestamp('timestamp')
-//         .default(sql`(CURRENT_TIMESTAMP)`)
-//         .notNull(),
-// });
-
-// export type InsertmovieImageCache = typeof movieImageCacheTable.$inferInsert;
-// export type SelectmovieImageCache = typeof movieImageCacheTable.$inferSelect;
-
-// // *** User ***
-// export const usersTable = mysqlTable('users', {
-//     id: bigint('id', {
-//         unsigned: true,
-//         mode: 'number',
-//     }).primaryKey(),
-//     username: varchar('username', { length: 30 }).notNull(),
-//     email: varchar('email', { length: 255 }).unique().notNull(),
-//     password: varchar('password', { length: 255 }).notNull(),
-//     createdAt: timestamp('created_at')
-//         .default(sql`(CURRENT_TIMESTAMP)`)
-//         .notNull(),
-//     lastLogin: timestamp('last_login').default(sql`(CURRENT_TIMESTAMP)`),
-// });
-
-// // *** Ratings ***
-// export const ratingsTable = mysqlTable('ratings', {
-//     id: serial('id').primaryKey(),
-//     userId: int('userId').notNull(),
-//     movieId: int('movieId').notNull(),
-//     rating: int('rating').notNull(),
-//     timestamp: timestamp('timestamp').default(sql`(CURRENT_TIMESTAMP)`),
-// });
-
-// export type InsertRatings = typeof ratingsTable.$inferInsert;
-// export type SelectRatings = typeof ratingsTable.$inferSelect;
-
-// *** SQLite ***
+// MySQL
 import { sql } from 'drizzle-orm';
-import { integer, sqliteTable, text, unique } from 'drizzle-orm/sqlite-core';
+import { bigint } from 'drizzle-orm/mysql-core';
+import {
+    int,
+    mysqlTable,
+    serial,
+    timestamp,
+    varchar,
+} from 'drizzle-orm/mysql-core';
 
 // *** Movie ***
-export const moviesTable = sqliteTable('movies', {
-    id: integer('id').primaryKey(),
-    title: text('title').notNull(),
-    genres: text('genres').notNull(),
+export const moviesTable = mysqlTable('movies', {
+    id: int('id').primaryKey(),
+    title: varchar('title', { length: 255 }).notNull(),
+    genres: varchar('genres', { length: 255 }).notNull(),
 });
 
 export type InsertMovie = typeof moviesTable.$inferInsert;
 export type SelectMovie = typeof moviesTable.$inferSelect;
 
-export const movieLinkIdTable = sqliteTable('movie_link_id', {
-    id: integer('id').primaryKey(),
-    imdbId: integer('imdbId').notNull(),
-    tmdbId: integer('tmdbId').notNull(),
+export const movieLinkIdTable = mysqlTable('movie_link_id', {
+    id: int('id').primaryKey(),
+    imdbId: int('imdbId').notNull(),
+    tmdbId: int('tmdbId').notNull(),
 });
 
 export type InsertMovieLinkId = typeof movieLinkIdTable.$inferInsert;
 export type SelectMovieLinkId = typeof movieLinkIdTable.$inferSelect;
 
-export const IMDBImageIdTable = sqliteTable('imdb_image_id', {
-    id: integer('id').primaryKey(),
-    imageId: integer('imageId').notNull(),
+export const IMDBImageIdTable = mysqlTable('imdb_image_id', {
+    id: int('id').primaryKey(),
+    imageId: int('imageId', { unsigned: true }).notNull(),
 });
 
 export type InsertIMDBImageId = typeof IMDBImageIdTable.$inferInsert;
 export type SelectIMDBImageId = typeof IMDBImageIdTable.$inferSelect;
 
-export const movieImageCacheTable = sqliteTable('movie_image_cache', {
-    id: integer('id').primaryKey(), // auto increment
-    movieId: integer('movieId')
+export const movieImageCacheTable = mysqlTable('movie_image_cache', {
+    id: serial('id').primaryKey(), // auto increment
+    movieId: int('movieId')
         .notNull()
         .references(() => moviesTable.id),
-    url: text('url').notNull(),
-    blurHash: text('blurHash'),
-    timestamp: text('timestamp')
+    url: varchar('url', { length: 255 }).notNull(),
+    blurHash: varchar('blurHash', { length: 255 }),
+    timestamp: timestamp('timestamp')
         .default(sql`(CURRENT_TIMESTAMP)`)
         .notNull(),
 });
@@ -124,38 +52,37 @@ export type InsertmovieImageCache = typeof movieImageCacheTable.$inferInsert;
 export type SelectmovieImageCache = typeof movieImageCacheTable.$inferSelect;
 
 // *** User ***
-export const usersTable = sqliteTable('users', {
-    id: integer('id').primaryKey(),
-    username: text('username').notNull(),
-    email: text('email').unique().notNull(),
-    password: text('password').notNull(),
-    createdAt: text('created_at')
+export const usersTable = mysqlTable('users', {
+    id: bigint('id', {
+        unsigned: true,
+        mode: 'number',
+    }).primaryKey(),
+    username: varchar('username', { length: 30 }).notNull(),
+    email: varchar('email', { length: 255 }).unique().notNull(),
+    password: varchar('password', { length: 255 }).notNull(),
+    createdAt: timestamp('created_at')
         .default(sql`(CURRENT_TIMESTAMP)`)
         .notNull(),
-    lastLogin: text('last_login').default(sql`(CURRENT_TIMESTAMP)`),
-    settings: text('settings').default(sql`'{}'`), // JSON stringified object
-    profileIcon: text('profile_icon'),
+    lastLogin: timestamp('last_login').default(sql`(CURRENT_TIMESTAMP)`),
+    profileIcon: varchar('profile_icon', { length: 255 }),
 });
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
-
-export const friendsTable = sqliteTable('friends', {
-    id: integer('id').primaryKey(),
-    userIdA: integer('userIdA').notNull(),
-    userIdB: integer('userIdB').notNull(),
-    status: integer('status').notNull(),
+export const friendsTable = mysqlTable('friends', {
+    id: int('id').primaryKey(),
+    userIdA: int('userIdA').notNull(),
+    userIdB: int('userIdB').notNull(),
+    status: int('status').notNull(),
 });
 
 export type InserFriend = typeof friendsTable.$inferInsert;
 export type SelectFriend = typeof friendsTable.$inferSelect;
 
-export const watchlistTable = sqliteTable('watchlist', {
-    id: integer('id').primaryKey(),
-    movieid: integer('movieId')
+export const watchlistTable = mysqlTable('watchlist', {
+    id: int('id').primaryKey(),
+    movieid: int('movieId')
         .notNull()
         .references(() => moviesTable.id),
-    userid: integer('userId')
+    userid: int('userId')
         .notNull()
         .references(() => usersTable.id),
 });
@@ -164,20 +91,19 @@ export type InsertWatchlist = typeof watchlistTable.$inferInsert;
 export type SelectWatchlist = typeof watchlistTable.$inferSelect;
 
 // *** Groups ***
-export const groupsTable = sqliteTable('groups', {
-    groupId: integer('groupId').notNull(),
-    groupName: text('groupName').notNull(),
-    adminId: integer('adminId').notNull(),
-    members: text('members').notNull(),
-    settings: text('settings').notNull(),
+export const groupsTable = mysqlTable('groups', {
+    groupId: int('groupId').notNull(),
+    groupName: varchar('groupName', { length: 16 }).notNull(),
+    adminId: int('adminId').notNull(),
+    members: varchar('members', { length: 100 }).notNull(),
 });
 
 export type InsertGroup = typeof groupsTable.$inferInsert;
 export type SelectGroup = typeof groupsTable.$inferSelect;
 
-export const groupInfoTable = sqliteTable('group_info', {
-    groupId: integer('group_id').primaryKey(),
-    groupAdmin: text('group_admin')
+export const groupInfoTable = mysqlTable('group_info', {
+    groupId: int('group_id').primaryKey(),
+    groupAdmin: varchar('group_admin', { length: 20 })
         .notNull()
         .references(() => usersTable.id), // foreign key to users table
 });
@@ -185,12 +111,12 @@ export const groupInfoTable = sqliteTable('group_info', {
 export type InsertGroupInfo = typeof groupInfoTable.$inferInsert;
 export type SelectGroupInfo = typeof groupInfoTable.$inferSelect;
 
-export const groupMembersTable = sqliteTable('group_members', {
-    id: integer('id').primaryKey(),
-    groupId: integer('group_id')
+export const groupMembersTable = mysqlTable('group_members', {
+    id: int('id').primaryKey(),
+    groupId: int('group_id')
         .notNull()
         .references(() => groupInfoTable.groupId), // foreign key to group_info table
-    userId: integer('user_id')
+    userId: int('user_id')
         .notNull()
         .references(() => usersTable.id), // foreign key to users table
 });
@@ -198,34 +124,176 @@ export const groupMembersTable = sqliteTable('group_members', {
 export type InsertGroupMember = typeof groupMembersTable.$inferInsert;
 export type SelectGroupMembers = typeof groupMembersTable.$inferSelect;
 
-export const groupRequestsTable = sqliteTable('groupRequests', {
-    id: integer('id').primaryKey(),
-    userId: integer('userId').notNull(),
-    groupId: integer('groupId').notNull(),
+export const groupRequestsTable = mysqlTable('groupRequests', {
+    id: int('id').primaryKey(),
+    userId: int('userId').notNull(),
+    groupId: int('groupId').notNull(),
 });
 
 export type InsertGroupRequestsTable = typeof groupRequestsTable.$inferInsert;
 export type SelectGroupRequestsTable = typeof groupRequestsTable.$inferSelect;
 
 // *** Ratings ***
-export const ratingsTable = sqliteTable(
-    'testRatings',
-    {
-        id: integer('id').primaryKey(),
-        userId: integer('userId').notNull(),
-        movieId: integer('movieId').notNull(),
-        rating: integer('rating').notNull(),
-        timestamp: text('timestamp')
-            .default(sql`(CURRENT_TIMESTAMP)`)
-            .notNull(),
-    },
-    (table) => ({
-        uniqueUserMovie: unique().on(table.userId, table.movieId), // Add UNIQUE constraint
-    })
-);
+export const ratingsTable = mysqlTable('ratings', {
+    id: serial('id').primaryKey(),
+    userId: int('userId').notNull(),
+    movieId: int('movieId').notNull(),
+    rating: int('rating').notNull(),
+    timestamp: timestamp('timestamp').default(sql`(CURRENT_TIMESTAMP)`),
+});
 
-export type InsertRatingsTable = typeof ratingsTable.$inferInsert;
-export type SelectRatingsTable = typeof ratingsTable.$inferSelect;
+export type InsertRatings = typeof ratingsTable.$inferInsert;
+export type SelectRatings = typeof ratingsTable.$inferSelect;
+
+// // *** SQLite ***
+// import { sql } from 'drizzle-orm';
+// import { integer, sqliteTable, text, unique } from 'drizzle-orm/sqlite-core';
+
+// // *** Movie ***
+// export const moviesTable = sqliteTable('movies', {
+//     id: integer('id').primaryKey(),
+//     title: text('title').notNull(),
+//     genres: text('genres').notNull(),
+// });
+
+// export type InsertMovie = typeof moviesTable.$inferInsert;
+// export type SelectMovie = typeof moviesTable.$inferSelect;
+
+// export const movieLinkIdTable = sqliteTable('movie_link_id', {
+//     id: integer('id').primaryKey(),
+//     imdbId: integer('imdbId').notNull(),
+//     tmdbId: integer('tmdbId').notNull(),
+// });
+
+// export type InsertMovieLinkId = typeof movieLinkIdTable.$inferInsert;
+// export type SelectMovieLinkId = typeof movieLinkIdTable.$inferSelect;
+
+// export const IMDBImageIdTable = sqliteTable('imdb_image_id', {
+//     id: integer('id').primaryKey(),
+//     imageId: integer('imageId').notNull(),
+// });
+
+// export type InsertIMDBImageId = typeof IMDBImageIdTable.$inferInsert;
+// export type SelectIMDBImageId = typeof IMDBImageIdTable.$inferSelect;
+
+// export const movieImageCacheTable = sqliteTable('movie_image_cache', {
+//     id: integer('id').primaryKey(), // auto increment
+//     movieId: integer('movieId')
+//         .notNull()
+//         .references(() => moviesTable.id),
+//     url: text('url').notNull(),
+//     blurHash: text('blurHash'),
+//     timestamp: text('timestamp')
+//         .default(sql`(CURRENT_TIMESTAMP)`)
+//         .notNull(),
+// });
+
+// export type InsertmovieImageCache = typeof movieImageCacheTable.$inferInsert;
+// export type SelectmovieImageCache = typeof movieImageCacheTable.$inferSelect;
+
+// // *** User ***
+// export const usersTable = sqliteTable('users', {
+//     id: integer('id').primaryKey(),
+//     username: text('username').notNull(),
+//     email: text('email').unique().notNull(),
+//     password: text('password').notNull(),
+//     createdAt: text('created_at')
+//         .default(sql`(CURRENT_TIMESTAMP)`)
+//         .notNull(),
+//     lastLogin: text('last_login').default(sql`(CURRENT_TIMESTAMP)`),
+//     settings: text('settings').default(sql`'{}'`), // JSON stringified object
+//     profileIcon: text('profile_icon'),
+// });
+
+// export type InsertUser = typeof usersTable.$inferInsert;
+// export type SelectUser = typeof usersTable.$inferSelect;
+
+// export const friendsTable = sqliteTable('friends', {
+//     id: integer('id').primaryKey(),
+//     userIdA: integer('userIdA').notNull(),
+//     userIdB: integer('userIdB').notNull(),
+//     status: integer('status').notNull(),
+// });
+
+// export type InserFriend = typeof friendsTable.$inferInsert;
+// export type SelectFriend = typeof friendsTable.$inferSelect;
+
+// export const watchlistTable = sqliteTable('watchlist', {
+//     id: integer('id').primaryKey(),
+//     movieid: integer('movieId')
+//         .notNull()
+//         .references(() => moviesTable.id),
+//     userid: integer('userId')
+//         .notNull()
+//         .references(() => usersTable.id),
+// });
+
+// export type InsertWatchlist = typeof watchlistTable.$inferInsert;
+// export type SelectWatchlist = typeof watchlistTable.$inferSelect;
+
+// // *** Groups ***
+// export const groupsTable = sqliteTable('groups', {
+//     groupId: integer('groupId').notNull(),
+//     groupName: text('groupName').notNull(),
+//     adminId: integer('adminId').notNull(),
+//     members: text('members').notNull(),
+//     settings: text('settings').notNull(),
+// });
+
+// export type InsertGroup = typeof groupsTable.$inferInsert;
+// export type SelectGroup = typeof groupsTable.$inferSelect;
+
+// export const groupInfoTable = sqliteTable('group_info', {
+//     groupId: integer('group_id').primaryKey(),
+//     groupAdmin: text('group_admin')
+//         .notNull()
+//         .references(() => usersTable.id), // foreign key to users table
+// });
+
+// export type InsertGroupInfo = typeof groupInfoTable.$inferInsert;
+// export type SelectGroupInfo = typeof groupInfoTable.$inferSelect;
+
+// export const groupMembersTable = sqliteTable('group_members', {
+//     id: integer('id').primaryKey(),
+//     groupId: integer('group_id')
+//         .notNull()
+//         .references(() => groupInfoTable.groupId), // foreign key to group_info table
+//     userId: integer('user_id')
+//         .notNull()
+//         .references(() => usersTable.id), // foreign key to users table
+// });
+
+// export type InsertGroupMember = typeof groupMembersTable.$inferInsert;
+// export type SelectGroupMembers = typeof groupMembersTable.$inferSelect;
+
+// export const groupRequestsTable = sqliteTable('groupRequests', {
+//     id: integer('id').primaryKey(),
+//     userId: integer('userId').notNull(),
+//     groupId: integer('groupId').notNull(),
+// });
+
+// export type InsertGroupRequestsTable = typeof groupRequestsTable.$inferInsert;
+// export type SelectGroupRequestsTable = typeof groupRequestsTable.$inferSelect;
+
+// // *** Ratings ***
+// export const ratingsTable = sqliteTable(
+//     'testRatings',
+//     {
+//         id: integer('id').primaryKey(),
+//         userId: integer('userId').notNull(),
+//         movieId: integer('movieId').notNull(),
+//         rating: integer('rating').notNull(),
+//         timestamp: text('timestamp')
+//             .default(sql`(CURRENT_TIMESTAMP)`)
+//             .notNull(),
+//     },
+//     (table) => ({
+//         uniqueUserMovie: unique().on(table.userId, table.movieId), // Add UNIQUE constraint
+//     })
+// );
+
+// export type InsertRatingsTable = typeof ratingsTable.$inferInsert;
+// export type SelectRatingsTable = typeof ratingsTable.$inferSelect;
 
 // To update the schema, run: npx drizzle-kit push
 // After deleting fts tables, run: CREATE VIRTUAL TABLE movies_fts USING fts5(id UNINDEXED, title, genres); INSERT INTO movies_fts (rowid, id, title, genres) SELECT id, id, title, genres FROM movies;
