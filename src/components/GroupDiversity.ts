7e4e44ff7abfe37c48dccf45449b1ccbd29208e3
Svolle <svolle777@icloud.com>
2025-05-05 02:24:44 +0200

    Too cooked to continue

--- src/components/GroupDiversity.ts
+++ src/components/GroupDiversity.ts
@@ -2,108 +2,65 @@
 used for weights contentbased % + collaborative % */
 import '@contentBasedFiltering-filtering/ts';
 import { getMovieById, movie } from '@/actions/movie/movie';
-import { moviesTable, testRatings } from '@/db/schema';
+import { groupsTable, moviesTable, testRatings } from '@/db/schema';
 import { db } from 'db';
 import { eq, ne } from 'drizzle-orm';
 import { getNamedRouteRegex } from 'next/dist/shared/lib/router/utils/route-regex';
 
 let targetUserRatings;
-export default async function similarityscore(
-    genreScoreMap: Map<string, number>,
-    group: number
+export default async function getGroupSimilarityScore(
+    groupId: number
 ): Promise<number> {
-    // fetch groupId and users from group table. (MISSING)
-    let similarityScore: number = 0;
-    let userId: number = targetUserRatings = 0;
-    // new map with userId as key and genre score map as value
+    // Fetch group members.
+    const users = await db
+        .select({ members: groupsTable.members })
+        .from(groupsTable)
+        .where(eq(groupsTable.groupId, groupId));
+
+    // Split members into array of IDs.
+    const memberIds = users[0].members.split('|');
+
+    // Map of user IDs and their genre ratings.
     const groupGenreScoreMap = new Map<number, Map<string, number>>();
-    for (const userId of group) {
-        const genreScoreMap = await genreData(userId);
-        groupGenreScoreMap.set(userId, genreScoreMap);
-    }
-    // aggregate genre difference for each genre (MISSING)
-        groupGenreScoreMap.forEach(userGenreScoreMap, userId) => {
-            // Compare logic - previus commit didnt work as intended
-        }}
 
-
-    type averageRating = {
-        runningTotal: number;
-        timesRated: number;
-    };
-
-    async function genreData(targetId: number): Promise<Map<string, number>> {
-        // Declare variable used for storing movie ratings.
-        let targetUserRatings;
-        targetUserRatings = await db
-            .select({
-                userId: testRatings.userId,
-                movieId: testRatings.movieId,
-                movieRating: testRatings.rating,
-            })
-            .from(testRatings)
-            .where(eq(testRatings.userId, targetId));
-
-        console.log('Got target user ratings.');
-        console.log(targetUserRatings);
-
-        // Find the total number of movies rated by the target user.
-        const totalMoviesRated = targetUserRatings!.length;
-
-        // Initialise genre score map.
-        const averageGenreRating = new Map<string, averageRating>();
-
-        // Iterate through each rating made by the target user.
-        for (const rating of targetUserRatings!) {
-            // Fetch the movie in question.
-            const movie = await getMovieById(rating.movieId);
-
-            // Check that the movie exists.
-            if (movie !== null) {
-                // Get genres of the movie.
-                const genres = movie.movieGenres;
-
-                // If movie has no genres, continue to the next rating/movie.
-                if (genres === '(no genres listed)') {
-                    continue;
-                }
-
-                // Split string into single genres.
-                const genreArray = genres.split('|');
-
-                // Iterate through each genre.
-                for (const genre of genreArray) {
-                    // If the genre score map does not contain an entry for the specified genre, initialise it.
-                    if (!averageGenreRating.has(genre)) {
-                        averageGenreRating.set(genre, {
-                            runningTotal: 0,
-                            timesRated: 0,
-                        });
-                    }
-
-                    // Update the score for the current genre.
-                    averageGenreRating.set(genre, {
-                        runningTotal:
-                            averageGenreRating.get(genre)!.runningTotal +
-                            rating.movieRating,
-                        timesRated:
-                            averageGenreRating.get(genre)!.timesRated + 1,
-                    });
-                }
-            }
-        }
-
-        // Convert running total and times rated to a score for each genre.
-        const genreScoreMap = new Map<string, number>();
-
-        for (const genre of averageGenreRating) {
-            genreScoreMap.set(
-                genre[0],
-                (genre[1].runningTotal / genre[1].timesRated) *
-                    (genre[1].timesRated / totalMoviesRated)
-            );
-        }
-        return genreScoreMap;
+    // For each member.
+    for (const userId of memberIds) {
+        // Get (and set) their genre scores.
+        const genreScoreMap = await getAllGenreScore(parseInt(userId));
+        groupGenreScoreMap.set(parseInt(userId), genreScoreMap);
     }
 }
 
+async function getAllGenreScore(userId: number): Promise<Map<string, number>> {
+    const map = new Map<string, number>();
+
+    // Define map for storing score.
+    const genreScores = new Map<string, rating>();
+
+    // Add every genre to map.
+    // Pre-determined list of all possible genres.
+
+    // Get all movies watched by user.
+    const movies = await db
+        .select({
+            movieId: testRatings.movieId,
+            movieRating: testRatings.rating,
+        })
+        .from(testRatings)
+        .where(eq(testRatings.userId, userId));
+
+    // For every movie.
+    for (const movie of movies) {
+        // Get movie object.
+        const m = await getMovieById(movie.movieId);
+
+        // Get array of genres.
+        const genres = m?.movieGenres.split('|');
+    }
+    // Get all genres for those movies, and find genre score.
+    // Set the corresponding map entry to the score.
+
+    // Return map.
+
+    return map;
+}
