ff8ef1edc1f72f4da2f38333ff75f37075adbaad
Svolle <svolle777@icloud.com>
2025-05-08 09:13:16 +0200

    Restructured function location

--- src/components/GroupDiversity.ts
+++ src/components/GroupDiversity.ts
@@ -5,6 +5,7 @@ import { groupsTable, ratingsTable } from '@/db/schema';
 import { db } from 'db';
 import { eq } from 'drizzle-orm';
 import { averageRating } from './ContentBasedFiltering/contentBasedFiltering';
+import cosineSimilarity from './cosineSimilarity/cosineSimilarity';
 
 export default async function getGroupSimilarityScore(
     groupId: number
@@ -135,38 +136,4 @@ async function getAllGenreScore(userId: number): Promise<number[]> {
 
     // Return the array.
     return genreScoreArray;
-}
-
-function cosineSimilarity(userA: number[], userB: number[]): number {
-    // Find dot product between the common ratings of user A and B.
-    let dotProduct = 0;
-
-    for (let i = 0; i < userA.length; i++) {
-        dotProduct += userA[i] * userB[i];
-    }
-
-    // Find the magnitude (Euclidean norm) of user A (square root of A transpose A).
-    let magnitudeA = 0;
-
-    // A transpose A.
-    for (let i = 0; i < userA.length; i++) {
-        magnitudeA += userA[i] * userA[i];
-    }
-
-    // Get the square root.
-    magnitudeA = Math.sqrt(magnitudeA);
-
-    // Find the magnitude (Euclidean norm) of user B (square root of B transpose B).
-    let magnitudeB = 0;
-
-    // B transpose B.
-    for (let i = 0; i < userB.length; i++) {
-        magnitudeB += userB[i] * userB[i];
-    }
-
-    // Get the square root.
-    magnitudeB = Math.sqrt(magnitudeB);
-
-    // Return the cosine similarity of the two users.
-    return dotProduct / (magnitudeA * magnitudeB);
 }
