56b2a4816b610fd70a913d1190f3f6462681a58f
FrederikSkipper <RK23KN@student.aau.dk>
2025-05-09 10:19:57 +0200

    Finished testing groupDiversity. cosineSimilarity is not tested here, must be in algortihm.

--- tests/user/groupDiversity.test.ts
+++ tests/user/groupDiversity.test.ts
@@ -1,5 +1,6 @@
 import getGroupSimilarityScore from '../../src/components/GroupDiversity';
 import { getAllGenreScore } from '../../src/components/GroupDiversity';
+// test cosineSimilarity when testing algorithms
 
 const SECONDS = 1000;
 jest.setTimeout(70 * SECONDS);
@@ -14,8 +15,14 @@ test('Returning a similarityscore for a custom picked group that should be very 
     expect(score).toBeCloseTo(0);
 });
 
-test('Returning an array of scores of a user who has only watched and rated western 5', async () => {
+test('Returning an array of scores of a user who has rated 15 westerns 5 popcorns', async () => {
     const array = await getAllGenreScore(2318340534);
-    expect(array[17]).toStrictEqual(5);
+    expect(array[17]).toBeCloseTo(5);
+});
+
+test('Returning an array of scores of a user who has rated 12 westerns (5,2,5,2....) and rated 3 comedy (5,4,3)', async () => {
+    const array = await getAllGenreScore(1253957859);
+    expect(array[17]).toBeCloseTo(2.8);
+    expect(array[3]).toBeCloseTo(0.8);
 });
 
